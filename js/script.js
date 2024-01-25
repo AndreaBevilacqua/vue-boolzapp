83059a72d55cc7707f36fad9818f9f04f80a0acc
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-25 11:29:48 +0100

    fixed code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 console.log('vue ok', Vue);
 
+const { user, contacts } = data;
+
 const {createApp} = Vue;
 const app = createApp({
     name: 'Boolzapp',
-    data: () => ({ ...data })
+    data: () => ({ 
+      user,
+      contacts,
+      currentId: 1,
+      newMessage: ''
+     }),
+
+     computed: {
+      currentContact() {
+
+      }
+     },
+
+     methods: {
+      getAvatarUrl({ avatar }) {
+        return `img/avatar${avatar}.jpg`
+      }
+     }
 })
 app.mount('#root');
 
